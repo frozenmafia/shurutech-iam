fa4a452c502c5ae2c08ff8e81f535c51a3d164c5
anchalshivank <anchalshivank99@gmail.com>
2024-05-27 10:33:45 +0530

    5th

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,5 +7,5 @@ client = TestClient(app)
 
 def test_get_home(test_client):
     response = test_client.get("/")
-    assert response.status_code == 400
+    assert response.status_code == 200
     assert response.json() == {"message": "Hello world"}
