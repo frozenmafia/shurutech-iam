8beec48b7c07d53b1d6d8c368f811e6403f9867a
anchalshivank <anchalshivank99@gmail.com>
2024-05-27 10:28:08 +0530

    4th:

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
